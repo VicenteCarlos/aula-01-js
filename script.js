a5a1440b7a5129e7cb0b5fc1e96642a99849a1e0
vicentecarlos <soniamariadias2012@hotmail.com>
2022-12-05 20:12:28 -0300

    inicia aula de js

---
+++ script.js
@@ -0,0 +1,92 @@
+/* ---- Variáveis ---- */
+// let
+// const
+// var
+
+//Tipos de dados
+// - String (Cadeia de caracteres)
+// - Number (Números)
+// - Boolean (Verdadeiro ou falso)
+
+//let nome = 'João Lemos', numero = 1, eVerdadeiro = false //true
+// console.log(nome)
+// console.log(numero)
+// console.log(eVerdadeiro)
+
+// Descobrindo a tipagem dos dados
+// console.log(typeof 1)
+
+// Operadores lógicos e aritiméticos
+/* + (soma)
+   - (subtração)
+   * (multiplicação)
+   / (Divisão)
+
+    = (atribuição)
+    == (Comparação do valor)
+    === (Comparação do valor e tipo)
+
+*/
+
+//    console.log('1' + 1) // Exemplo de concatenção ou junção
+
+/* ---- Estruturas codicionais ---- */
+
+// Há dois tipos, sendo eles: if e o "?" (ternário)
+
+// let numero = 2
+
+// if ('2' === 2) {
+//     // Caso dentro dos parênteses seja true, esse bloco de código vai ser executado
+//     console.log("Número dois")
+// } else {
+//     console.log("Não é o número " + 2)
+// }
+
+// let numero = 2
+
+// if (numero == 0) {
+//     console.log(`O número é ${numero}`)
+// } else if (numero == 1) {
+//     console.log(`1 é igual a ${numero}`)
+// } else {
+//     console.log("Não é 1 e nem zero")
+// }
+
+// Ternário, é uma forma resumida de fazer uma estrutura condicional
+
+// console.log(1 == 0 ? true : false)
+
+// if (1 == 1) {
+//     //true
+// } else {
+//     //false
+// }
+
+// Estruturas de repetição (for, while, do while)
+
+
+// for (let contador = 1; contador <= 100; contador++) {
+//     console.log(`Contador: ${contador}`)
+// }
+
+let contador = 1
+
+// while (contador <= 5) {
+//     console.log(contador)
+//     contador++
+// }
+
+do {
+    console.log(contador)
+    contador++
+} while (contador <= 10)
+
+
+
+
+
+
+
+
+/* ---- Desconhecido... ---- */
